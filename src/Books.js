b219914955e38217905608d71799a2d467563fbf
Alexandre-Furstenberger <alexandre.furstenberger@gmail.com>
2018-04-15 09:56:15 -0300

    FEAT: Page renders books inside ordered list

--- src/Books.js
+++ src/Books.js
@@ -21,26 +21,28 @@ class Books extends Component {
         const bookList = this.props.bookList;
 
         return ( 
-            <div>
+            <ol className="books-grid">
                 {bookList.map((book) => (
-                    <div className="book" key={book.id}>
-                        <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                                <select>
-                                    <option value="none" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
+                    <li>    
+                        <div className="book" key={book.id}>
+                            <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                <div className="book-shelf-changer">
+                                    <select>
+                                        <option value="none" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
                             </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors.join(', ')}</div>
                         </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors.join(', ')}</div>
-                    </div>
+                    </li>
                 ))}
-            </div>
+            </ol>
         );
     }
 }
