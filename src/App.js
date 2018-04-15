6556e1407388bca9efe1990a27f3acb62f7677ad
Alexandre-Furstenberger <alexandre.furstenberger@gmail.com>
2018-04-15 16:16:57 -0300

    FEAT: Ádd page routers for book search

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        <Route path={"/search"} render={ () => (
           <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
@@ -40,7 +40,8 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        )}/>
+        <Route exact path="/" render={ () => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -50,7 +51,7 @@ class BooksApp extends React.Component {
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
           </div>
-        )}
+        )}/>
       </div>
     )
   }
